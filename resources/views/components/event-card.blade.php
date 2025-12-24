01945075a90ff85ba57ed492fa85ca91c9d8bb30
Jessch22 <jessica.535220187@stu.untar.ac.id>
2025-12-25 02:03:20 +0700

    update data

--- resources/views/components/event-card.blade.php
+++ resources/views/components/event-card.blade.php
@@ -1,21 +1,25 @@
 @props(['item'])
 
+@php
+    $gambar = $item['gambar_event'];
+    $isLink = \Illuminate\Support\Str::startsWith($gambar, ['http://', 'https://']);
+    $finalSrc = $isLink ? $gambar : asset($gambar);
+@endphp
+
 <div class="event-card">
     <div class="image-wrapper">
-        <img src="{{ $item['gambar_event'] }}" alt="{{ $item['nama_event'] ?? 'Event' }}" class="event-image">
+        <img src="{{ $finalSrc }}" alt="{{ $item['nama_event'] ?? 'Event' }}" class="event-image">
         
         <span class="service-badge">{{ $item['jenis_service'] ?? 'Layanan' }}</span>
     </div>
-
+    
     <div class="event-details">
-        <!-- Jenis Event -->
         <h3 class="event-tag">{{ $item['jenis_event'] }}</h3>
         
         @if(!empty($item['nama_event']))
             <p class="event-name">{{ $item['nama_event'] }}</p>
         @endif
 
-        <!-- Lokasi -->
         <h3 class="event-location">
             <i class="fa-solid fa-location-dot"></i> {{ $item['lokasi_event'] }}
         </h3>
