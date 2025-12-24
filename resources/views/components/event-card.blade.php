8b80814315e95b9e8294d8fedf84fc126c9c1ff6
Jessch22 <jessica.535220187@stu.untar.ac.id>
2025-12-25 01:48:23 +0700

    update event card

--- resources/views/components/event-card.blade.php
+++ resources/views/components/event-card.blade.php
@@ -1,8 +1,12 @@
 @props(['item'])
 
 <div class="event-card">
-    <img src="{{ $item['gambar_event'] }}" alt="{{ $item['nama_event'] ?? 'Event' }}" class="event-image">
-    
+    <div class="image-wrapper">
+        <img src="{{ $item['gambar_event'] }}" alt="{{ $item['nama_event'] ?? 'Event' }}" class="event-image">
+        
+        <span class="service-badge">{{ $item['jenis_service'] ?? 'Layanan' }}</span>
+    </div>
+
     <div class="event-details">
         <!-- Jenis Event -->
         <h3 class="event-tag">{{ $item['jenis_event'] }}</h3>
