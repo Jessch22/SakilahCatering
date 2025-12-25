ee1e00f8a47abf65091728588b44eb04f754b92b
Jessch22 <jessica.535220187@stu.untar.ac.id>
2025-12-25 16:22:14 +0700

    update home

--- resources/views/components/card-hover.blade.php
+++ resources/views/components/card-hover.blade.php
@@ -1,7 +1,14 @@
-<div class="card-hover" style="background-image: url('https://cdn.pixabay.com/photo/2016/11/23/15/48/audience-1853662_1280.jpg');">
+@props(['item', 'whatsappLink'])
+
+<div class="card-hover" style="background-image: url('{{ asset($item['image']) }}');">
     <div class="card-content">
-      <div class="card-title">Paket Catering</div>
-      <div class="card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed</div>
-      <a href="#" class="card-btn">Lihat Selengkapnya</a>
+        <div class="card-title">{{ $item['title'] }}</div>
+        <div class="card-desc">{{ $item['desc'] }}</div>
+        
+        <a href="{{ $whatsappLink }}&text=Halo%20Sakilah%20Catering,%20saya%20tertarik%20dengan%20{{ urlencode($item['title']) }}" 
+           class="card-btn" 
+           target="_blank">
+            <i class="fa-brands fa-whatsapp"></i> {{ $item['button_text'] }}
+        </a>
     </div>
-  </div>
+</div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -16,108 +16,90 @@
 <div class="hero-banner">
   <div class="hero-overlay"></div>
   <div class="hero-content">
-    <h1 class="hero-title">Sakilah Catering</h1>
-    <div class="hero-slogan">Wedding Organizer & Catering Service</div>
-    <a href="https://wa.me/+6285183098884" class="hero-btn">Pesan Sekarang</a>
+    <h1 class="hero-title">{{ $hero['title'] }}</h1>
+    <div class="hero-slogan">{{ $hero['slogan'] }}</div>
+    <a href="{{ $waLink }}?text=Halo%20Sakilah%20Catering,%20saya%20mau%20tanya-tanya" class="hero-btn" target="_blank">
+        {{ $hero['button_text'] }}
+    </a>
   </div>
 </div>
 @endsection
 
 
 @section('content')
-
+<!-- 1. TENTANG KAMI -->
 <div class="white-section">
   <div class="title-white-section">
     <h2>Tentang Kami</h2>
     <p class="accent-content"></p>
   </div>
   <div class="about-us-content">
-    <img class="about-us-image" src="{{ asset('img/asset/IMG-20250927-WA0054.jpg') }}" alt="">
+    <img class="about-us-image" src="{{ asset($about['image']) }}" alt="Tentang Sakilah Catering">
     <div class="about-us-keterangan">
         <div class="about-us-isi">
-          <p>
-              <strong>Sakilah Catering</strong>, di bawah naungan CV. Sakilah Cahaya Kharisma, hadir sebagai mitra layanan catering dan event organizer profesional yang berbasis di Jati Asih, Bekasi.
-          </p>
-
-          <p>
-          Sakilah melayani seluruh wilayah Jabodetabek dengan tawaran paket beragam:
-          </p>
-          <ul>
-              <li>
-                <strong>Paket Catering Hemat:</strong> Mulai Rp28.000/porsi (Sudah termasuk alat & waiters)
-              </li>
-              <li>
-                <strong>All-In Wedding Package:</strong> Mulai Rp60 Juta (Lengkap dengan Venue, Dekorasi, & WO)
-              </li>
-          </ul>
-          <br>
-      </div>
-      <a href="{{ url('/about-us') }}" class="about-us-btn">Selengkapnya</a>
+          {!! $about['content'] !!}
+        </div>
+      <a href="{{ $about['button_url'] }}" class="about-us-btn">{{ $about['button_text'] }}</a>
     </div>
   </div>
 </div>
 
+<!-- 2. PORTOFOLIO -->
 <div class="blue-section cp">
-  <h2 class="center">Portofolio Catering</h2>
+  <h2 class="center">{{ $portfolio['title'] }}</h2>
   <div class="cp-wrapper">
     <button class="prev"><i class="fas fa-chevron-left"></i></button>
     <div class="cp-container">
-      <img src="{{ asset('img/asset/IMG-20250927-WA0055.jpg') }}">
-      <img src="{{ asset('img/asset/IMG-20250927-WA0094.jpg') }}">
-      <img src="{{ asset('img/asset/IMG-20250927-WA0090.jpg') }}">
+      @foreach($portfolio['images'] as $img)
+        <img src="{{ asset($img) }}" alt="Portofolio Event">
+      @endforeach
     </div>
     <button class="next"><i class="fas fa-chevron-right"></i></button>
   </div>
   
   <div class="center" style="margin-top: 40px;">
-    <a href="{{ url('/event') }}" class="btn-gallery">Lihat Galeri Lainnya</a>
+    <a href="{{ $portfolio['button_url'] }}" class="btn-gallery">{{ $portfolio['button_text'] }}</a>
   </div>
 </div>
 
+<!-- 3. LAYANAN KAMI -->
 <div class="white-section service-section">
   <div class="title-white-section">
-    <h2>Paket Kami</h2>
+    <h2>Layanan Kami</h2>
     <p class="accent-content"></p>
   </div>
   <div class="card-hover-wrapper">
     <div class="card-hover-container service-container">
-      
-      <div class="card-hover" style="background-image: url('https://cdn.pixabay.com/photo/2016/11/23/15/48/audience-1853662_1280.jpg');">
-        <div class="card-content">
-          <div class="card-title">PAKET CATERING ONLY</div>
-          <div class="card-desc">Mulai dari Rp28.000/pax!</div>
-          <a href="#" class="card-btn">Tanya Selengkapnya</a>
-        </div>
-      </div>
-
-      <div class="card-hover" style="background-image: url('https://cdn.pixabay.com/photo/2016/11/23/15/48/audience-1853662_1280.jpg');">
-        <div class="card-content">
-          <div class="card-title">PAKET ALL-IN WEDDING</div>
-          <div class="card-desc">Mulai dari Rp60juta!</div>
-          <a href="#" class="card-btn">Tanya Selengkapnya</a>
-        </div>
-      </div>
+      @foreach($services as $item)
+        <x-card-hover 
+            :item="$item" 
+            :whatsappLink="$waLink . '?'" 
+        />
+      @endforeach
 
     </div>
     <div class="scroll-dots"></div>
     </div>
 </div>
 
+<!-- 4. TESTIMONI -->
 <div class="blue-section testi">
   <h2 class="center">Testimoni</h2>
   <div class="video-container">
+    
+    @foreach($testimonials as $link)
     <div class="instagram-wrapper">
-      <blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/reel/DSRGrhZATRR/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14">
+      <blockquote class="instagram-media" 
+        data-instgrm-permalink="{{ $link }}" 
+        data-instgrm-version="14">
       </blockquote>
     </div>
+    @endforeach
 
-    <div class="instagram-wrapper">
-      <blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/reel/DR32x8lgcTC/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14">
-      </blockquote>
-    </div>
   </div>
 </div>
 
+<!-- 5. SUPPORTED EVENT -->
 <div class="white-section">
   <div class="title-white-section">
     <h2>Supported Event</h2>
@@ -126,22 +108,19 @@
   <x-logo-marquee />
 </div>
 
+<!-- 6. ACHIEVEMENTS -->
 <div class="blue-section achievements">
   <h2>Our Achievements</h2>
   <br>
   <div class="achievements-list">
-      <div class="achievement-item">
-        <span class="achievement-main">2</span>
-        <span class="achievement-desc">Tahun Pengalaman</span>
-    </div>
+    
+    @foreach($achievements as $ach)
     <div class="achievement-item">
-        <span class="achievement-main">100+</span>
-        <span class="achievement-desc">Acara Dilayani</span>
+        <span class="achievement-main">{{ $ach['number'] }}</span>
+        <span class="achievement-desc">{{ $ach['desc'] }}</span>
     </div>
-    <div class="achievement-item">
-        <span class="achievement-main">100%</span>
-        <span class="achievement-desc">Halal & Higienis</span>
-    </div>
+    @endforeach
+
   </div>
 </div>
 @endsection
