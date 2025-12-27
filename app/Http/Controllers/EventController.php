6c51ccb65ac3ed71e13bd4804a2bfa13b5f162f1
Jessch22 <jessica.535220187@stu.untar.ac.id>
2025-12-28 02:22:38 +0700

    Update Event Page

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -15,22 +15,22 @@ class EventController extends Controller
         $dataEvents = EventsData::getAll();
 
         // 2. Konversi Array biasa menjadi Collection Laravel
-        $collection = new Collection($dataEvents);
+        $collection = collect($dataEvents);
 
         // 3. Konfigurasi Pagination
         $perPage = 9;
         $currentPage = LengthAwarePaginator::resolveCurrentPage();
         
         // 4. Potong data sesuai halaman
-        $currentPageItems = $collection->slice(($currentPage - 1) * $perPage, $perPage)->all();
+        $currentPageItems = $collection->slice(($currentPage - 1) * $perPage, $perPage)->values();
 
         // 5. Buat Object Paginator
         $paginatedEvents = new LengthAwarePaginator(
             $currentPageItems, 
-            count($collection), // Total semua data
-            $perPage, // Item per halaman
-            $currentPage, // Halaman saat ini
-            ['path' => $request->url(), 'query' => $request->query()] // Agar link halaman (page=2) berfungsi
+            count($collection), 
+            $perPage, 
+            $currentPage, 
+            ['path' => $request->url(), 'query' => $request->query()]
         );
 
         // 6. Kirim ke View
--- resources/views/components/event-card.blade.php
+++ resources/views/components/event-card.blade.php
@@ -1,27 +1,33 @@
 @props(['item'])
 
 @php
-    $gambar = $item['gambar_event'];
-    $isLink = \Illuminate\Support\Str::startsWith($gambar, ['http://', 'https://']);
-    $finalSrc = $isLink ? $gambar : asset($gambar);
+    $imgSrc = $item->gambar_event; // Akses pakai -> (Object)
+    
+    if (\Illuminate\Support\Str::startsWith($imgSrc, 'http')) {
+        $finalImage = $imgSrc;
+    } elseif (\Illuminate\Support\Str::startsWith($imgSrc, 'img/')) {
+        $finalImage = asset($imgSrc);
+    } else {
+        $finalImage = asset('storage/' . $imgSrc);
+    }
 @endphp
 
 <div class="event-card">
     <div class="image-wrapper">
-        <img src="{{ $finalSrc }}" alt="{{ $item['nama_event'] ?? 'Event' }}" class="event-image">
+        <img src="{{ $finalImage }}" alt="{{ $item->nama_event }}" class="event-image">
         
-        <span class="service-badge">{{ $item['jenis_service'] ?? 'Layanan' }}</span>
+        <span class="service-badge">{{ $item->jenis_service }}</span>
     </div>
     
     <div class="event-details">
-        <h3 class="event-tag">{{ $item['jenis_event'] }}</h3>
+        <h3 class="event-tag">{{ $item->jenis_event }}</h3>
         
-        @if(!empty($item['nama_event']))
-            <p class="event-name">{{ $item['nama_event'] }}</p>
+        @if(!empty($item->nama_event))
+            <p class="event-name">{{ $item->nama_event }}</p>
         @endif
 
         <h3 class="event-location">
-            <i class="fa-solid fa-location-dot"></i> {{ $item['lokasi_event'] }}
+            <i class="fa-solid fa-location-dot"></i> {{ $item->lokasi_event }}
         </h3>
     </div>
 </div>
